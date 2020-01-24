c689c99e0f8026f6fbbf23acb66e2ea81969ebb9
MusH <collinsmucheru@gmail.com>
2020-01-24 16:36:35 +0300

    basic test for the endpoints (helps keep code working in case a refactor is required)

--- test/bleacher_report_api_web/controllers/reaction_controller_test.exs
+++ test/bleacher_report_api_web/controllers/reaction_controller_test.exs
@@ -1,60 +1,66 @@
 defmodule BleacherReportApiWeb.ReactionControllerTest do
   use BleacherReportApiWeb.ConnCase
 
-  alias BleacherReportApi.Reactions
+  # alias BleacherReportApi.Reactions
 
-  @create_attrs %{
-    action: "some action",
+  @add_valid_reaction %{
+    action: "add",
+    reaction_type: "fire",
+    type: "reaction",
     content_id: "some content_id",
-    reaction_type: "some reaction_type",
-    type: "some type",
     user_id: "some user_id"
   }
-  @update_attrs %{
-    action: "some updated action",
-    content_id: "some updated content_id",
-    reaction_type: "some updated reaction_type",
-    type: "some updated type",
-    user_id: "some updated user_id"
+  @remove_valid_reaction %{
+    action: "remove",
+    reaction_type: "fire",
+    type: "reaction",
+    content_id: "some content_id",
+    user_id: "some user_id"
   }
-  @invalid_attrs %{action: nil, content_id: nil, reaction_type: nil, type: nil, user_id: nil}
-
-  def fixture(:reaction) do
-    {:ok, reaction} = Reactions.create_reaction(@create_attrs)
-    reaction
-  end
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "create reaction" do
-    test "renders reaction when data is valid", %{conn: conn} do
-      #      conn = post(conn, Routes.reaction_path(conn, :create), reaction: @create_attrs)
-      #      assert %{"id" => id} = json_response(conn, 201)["data"]
-      #
-      #      conn = get(conn, Routes.reaction_path(conn, :show, id))
-      #
-      #      assert %{
-      #               "id" => id,
-      #               "action" => "some action",
-      #               "content_id" => "some content_id",
-      #               "reaction_type" => "some reaction_type",
-      #               "type" => "some type",
-      #               "user_id" => "some user_id"
-      #             } = json_response(conn, 200)["data"]
-      :ok
+  describe "user reacts to content" do
+    test "renders added reaction when data provided is valid", %{conn: conn} do
+      conn = post(conn, Routes.reaction_path(conn, :react), @add_valid_reaction)
+
+      assert %{
+               "action" => "add",
+               "content_id" => "some content_id",
+               "reaction_type" => "fire",
+               "type" => "reaction",
+               "user_id" => "some user_id"
+             } = json_response(conn, 201)["data"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      #      conn = post(conn, Routes.reaction_path(conn, :create), reaction: @invalid_attrs)
-      #      assert json_response(conn, 422)["errors"] != %{}
-      :ok
+    test "renders removed reaction when data provided is valid", %{conn: conn} do
+      # setup (add a reaction to remove) -> typically done using fixtures
+      post(conn, Routes.reaction_path(conn, :react), @add_valid_reaction)
+      # remove added reaction
+      conn = post(conn, Routes.reaction_path(conn, :react), @remove_valid_reaction)
+
+      assert %{
+               "action" => "remove",
+               "content_id" => "some content_id",
+               "reaction_type" => "fire",
+               "type" => "reaction",
+               "user_id" => "some user_id"
+             } = json_response(conn, 201)["data"]
     end
   end
 
-  defp create_reaction(_) do
-    reaction = fixture(:reaction)
-    {:ok, reaction: reaction}
+  describe "count users' reaction to content" do
+    test "return count of fire reactions", %{conn: conn} do
+      # setup (add a reaction to count) -> typically done using fixtures
+      post(conn, Routes.reaction_path(conn, :react), @add_valid_reaction)
+
+      conn =
+        get(conn, Routes.reaction_path(conn, :count_reactions, @add_valid_reaction.content_id))
+
+      %{"content_id" => "some content_id", "reactions_count" => %{"fire" => 1}} =
+        json_response(conn, 200)
+    end
   end
 end
