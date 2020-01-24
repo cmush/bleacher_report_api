eceab51964c2e030cda4be8db0cf24c5c4141202
MusH <collinsmucheru@gmail.com>
2020-01-24 15:26:06 +0300

    add reactions contexts - interfaces with the Cache Wrapper (and Persistent Storage in future)

--- lib/bleacher_report_api/reactions.ex
+++ lib/bleacher_report_api/reactions.ex
@@ -2,20 +2,65 @@ defmodule BleacherReportApi.Reactions do
   @moduledoc """
   The Reactions context.
   """
+  alias BleacherReportApi.ReactionsCacheWrapper, as: RCWrapper
 
   @doc """
-  Creates a reaction.
+  Adds a reaction.
 
   ## Examples
 
-      iex> create_reaction(%{field: value})
-      {:ok, %Reaction{}}
+      iex> add_reaction(%{} = reaction)
+      {:ok, key}
 
-      iex> create_reaction(%{field: bad_value})
+      iex> add_reaction(%{field: bad_value})
       {:error, ...}
 
   """
-  def create_reaction(attrs \\ %{}) do
-    raise "TODO"
+  def add_reaction(
+        %{
+          "action" => "add",
+          "content_id" => content_id,
+          "reaction_type" => "fire",
+          "type" => "reaction",
+          "user_id" => user_id
+        } = reaction_params
+      ) do
+    ## Store the reaction in the persistent database (such as Postgres) TODO
+    # Reactions.create(reaction) # call to context (linked to ecto/postgres/mysql)
+    ## The assumptions here are that the relevant user (user_id) and content (content_id) already exist in the
+    ## persistent database into which we will record the reaction. Additionally, there exists a relationship
+    ## between reactions/users and reactions/content. i.e, no reaction can be recorded from a nonexistent user
+    ## or to nonexistent content.
+
+    ## Since we are skipping the database, we use the cache for temporary storage
+    RCWrapper.add_cache_reaction({content_id, user_id}, reaction_params)
+  end
+
+  @doc """
+  Removes a reaction.
+
+  ## Examples
+
+      iex> remove_reaction(%{} = reaction)
+      {:ok, key}
+
+      iex> remove_reaction(%{field: bad_value})
+      {:error, ...}
+
+  """
+  def remove_reaction(
+        %{
+          "action" => "remove",
+          "content_id" => content_id,
+          "reaction_type" => "fire",
+          "type" => "reaction",
+          "user_id" => user_id
+        }
+      ) do
+    RCWrapper.remove_cache_reaction({content_id, user_id})
+  end
+
+  def list_content_reactions(content_id) do
+    RCWrapper.list_by_content_id(content_id)
   end
 end
